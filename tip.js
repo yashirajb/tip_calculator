1538ecf1b79c4415d55c78199ca22c4ccb2f7e8a
Yashira Baggett <Yashira@Yashiras-Air.attlocal.net>
2021-03-15 18:28:05 -0700

    js code finished, working out bug

--- tip.js
+++ tip.js
@@ -1,7 +1,17 @@
- const range = document.getElementById("");
- const tipOut = document.getElementById("");
- const btnCalculate = document.getElementById("");
- const billAmount = document.getElementById("");
- const resultArea = document.getElementById("");
+ const range = document.getElementById("tipRange");
+ const tipOut = document.getElementById("percentOut");
+ const btnCalculate = document.getElementById("btnCalculate");
+ const billAmount = document.getElementById("billAmount");
+ const resultArea = document.getElementById("result");
 
- 
+ range.ondrag = function(){
+     tipOut.innerHTML = range.value + "%";
+ }
+
+ btnCalculate.onclick = function(){
+     let theBill = parseFloat(billAmount.value);
+     let tip = (theBill + (range.value/100)).toFixed(2);
+     let total = (theBill + parseFloat(tip)).toFixed(2);
+     let out = `<strong>Tip Amount:</strong> $${tip}<br><strong>Total Bill:</strong> $${total}</br>`
+     resultArea.innerHTML = out;
+ }
