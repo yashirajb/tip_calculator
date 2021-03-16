71432673f9c73b0946bfc5ce47370f8af2850bf3
Yashira Baggett <Yashira@Yashiras-Air.attlocal.net>
2021-03-15 17:51:46 -0700

    adding js code

---
+++ tip.js
@@ -0,0 +1,7 @@
+ const range = document.getElementById("");
+ const tipOut = document.getElementById("");
+ const btnCalculate = document.getElementById("");
+ const billAmount = document.getElementById("");
+ const resultArea = document.getElementById("");
+
+ 
